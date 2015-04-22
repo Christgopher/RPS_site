11209fc7b49b34047e36817b3a07d59871ebafd4
Guest User <student@epicodus.com>
2015-04-22 11:52:43 -0700

    will return true or false for RPS matchups

--- lib/RPS.rb
+++ lib/RPS.rb
@@ -1,9 +1,15 @@
 class String
   define_method(:beats?) do |p_two|
-    rock = {"scissors" => "blue", "paper" => false, "rock" => false}
+    rock = {"scissors" => true, "paper" => false, "rock" => false}
+    paper = {"scissors" => false, "paper" => false, "rock" => true}
+    scissors = {"scissors" => false, "paper" => true, "rock" => false}
     p_one = self
     if p_one.==("rock")
           rock.fetch(p_two)
+        elsif p_one.==("scissors")
+          scissors.fetch(p_two)
+        else
+          paper.fetch(p_two)
     end
   end
 end
--- spec/RPS_spec.rb
+++ spec/RPS_spec.rb
@@ -1,8 +1,11 @@
 require('rspec')
 require('RPS')
 
-describe("") do
+describe('String#beats?') do
   it("returns true if rock is the object and scissors is the argument") do
     expect("rock".beats?("scissors")).to(eq(true))
+  end
+  it("returns true if scissors is the object and paper is the argument") do
+    expect("scissors".beats?("paper")).to(eq(true))
   end
 end
